fa08681cb81eca371cce3bc16901002b59d5436f
aditya109 <adikid1996@gmail.com>
2020-09-21 20:19:23 +0530

    init repo
    Signed-off-by: aditya109 <adikid1996@gmail.com>

---
+++ src/test/java/io/github/aditya109/springbootjokesapp/SpringBootJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aditya109.springbootjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
